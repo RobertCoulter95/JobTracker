2e9c59782d7beff0ef767e614078c48eea5e8a8c
Robert.Coulter <Robert.Coulter@azaleahealth.com>
2023-03-28 19:23:56 -0400

    Updates to company

--- src/main/java/com/example/jobtracker/company/CompanyController.java
+++ src/main/java/com/example/jobtracker/company/CompanyController.java
@@ -1,5 +1,9 @@
 package com.example.jobtracker.company;
 
+import com.example.jobtracker.job.Job;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class CompanyController {
     CompanyService companyService;
 
+    @Autowired
+    public CompanyController(CompanyService companyService) {this.companyService = companyService;}
+
+    @PostMapping
+    public void registerNewCompany(@RequestBody Company company){
+        companyService.addNewCompany(company);
+    }
 }
--- src/main/java/com/example/jobtracker/company/CompanyService.java
+++ src/main/java/com/example/jobtracker/company/CompanyService.java
@@ -1,10 +1,13 @@
 package com.example.jobtracker.company;
 
+import com.example.jobtracker.job.Job;
 import com.example.jobtracker.job.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CompanyService {
@@ -13,5 +16,13 @@ public class CompanyService {
     @Autowired
     public CompanyService(CompanyRepository companyRepository){
         this.companyRepository = companyRepository;
+    }
+
+    public void addNewCompany(Company company) {
+        Optional<Company> companyOptional = companyRepository.findByName(company.getName()); // Short term solution. How will we handle companies with the same name? (Small shops)
+        if (companyOptional.isPresent()){
+            throw new IllegalStateException("Company with id: " + companyOptional.get().getId() + " already exists"); // Should return a link for the company
+        }
+        companyRepository.save(company);
     }
 }
