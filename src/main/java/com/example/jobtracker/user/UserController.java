46fe015ac33af1e9d14b4c887d837487adf1d706
Robert.Coulter <Robert.Coulter@azaleahealth.com>
2023-03-28 19:21:20 -0400

    Created user with controller and service

---
+++ src/main/java/com/example/jobtracker/user/UserController.java
@@ -0,0 +1,22 @@
+package com.example.jobtracker.user;
+
+import com.example.jobtracker.job.Job;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/user")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public void registerNewUser(@RequestBody User user) {userService.createUser(user);}
+
+
+}
